90de0f2ec1a39a96e4879e039172a47714c6623b
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2024-10-20 01:51:24 +0200

    fix async authorization handler warning

--- src/WeatherMonitor/WeatherMonitor.Server.UserAuthorization/Features/UserIsAdminRequirementHandler.cs
+++ src/WeatherMonitor/WeatherMonitor.Server.UserAuthorization/Features/UserIsAdminRequirementHandler.cs
@@ -12,42 +12,43 @@ internal class UserIsAdminRequirementHandler : AuthorizationHandler<UserIsAdminR
         _logger = logger;
     }
 
-    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, UserIsAdminRequirement requirement)
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, UserIsAdminRequirement requirement)
     {
         var userIdClaim = context.User.Claims.FirstOrDefault(x => x.Type == requirement.UserIdClaimTypeName);
         if (string.IsNullOrEmpty(userIdClaim?.Value))
         {
             context.Fail();
-            return;
+            return Task.CompletedTask;
         }
 
         var roleClaim = context.User.Claims.FirstOrDefault(x => x.Type == requirement.RoleClaimTypeName);
         if (string.IsNullOrEmpty(roleClaim?.Value))
         {
             context.Fail();
-            return;
+            return Task.CompletedTask;
         }
 
         if (!int.TryParse(roleClaim.Value, out var roleTypeValue))
         {
             context.Fail();
-            return;
+            return Task.CompletedTask;
         }
 
         if (!Enum.IsDefined(typeof(Role), roleTypeValue))
         {
             context.Fail();
-            return;
+            return Task.CompletedTask;
         }
 
         var roleEnumResult = (Role)roleTypeValue;
         if (!requirement.AdminRoleTypes.Any(role => role == roleEnumResult))
         {
             context.Fail();
-            return;
+            return Task.CompletedTask;
         }
 
         _logger.LogInformation("User: {UserId} with admin privileges invoking action", userIdClaim);
         context.Succeed(requirement);
+        return Task.CompletedTask;
     }
 }
