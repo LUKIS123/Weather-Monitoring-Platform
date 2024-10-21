396cf4022a76f52a31b7e824150c2d814e678bef
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2024-10-22 00:49:31 +0200

    init mqtt client

---
+++ src/WeatherMonitorCore/WeatherMonitorCore.Shared.MqttClient/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace WeatherMonitorCore.Shared.MqttClient;
+
+public class Class1
+{
+
+}
+// PS C:\Users\lukas> mosquitto_pub -h localhost -p 1883 -t "weather/F28B0755-587D-4F9D-8FF6-ECC7277C05F2" -m "Hello MQTT3" -u "odbieracz" -P "5BFJWzxWnn0WP8xZ" -i "ODBIERACZ-A916C8DD-6A31-43EC-B9ED-D6F48A0C46FC"
