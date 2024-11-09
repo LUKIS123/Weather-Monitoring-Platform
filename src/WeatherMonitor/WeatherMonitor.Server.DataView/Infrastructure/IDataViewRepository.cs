f4ca9356c8b49a23e5287a2df22946e5b2ca454f
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2024-11-09 23:03:24 +0100

    adjustments

--- src/WeatherMonitor/WeatherMonitor.Server.DataView/Infrastructure/IDataViewRepository.cs
+++ src/WeatherMonitor/WeatherMonitor.Server.DataView/Infrastructure/IDataViewRepository.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WeatherMonitor.Server.DataView.Infrastructure;
-internal interface IDataViewRepository
+﻿namespace WeatherMonitor.Server.DataView.Infrastructure;
+public interface IDataViewRepository
 {
+
 }
