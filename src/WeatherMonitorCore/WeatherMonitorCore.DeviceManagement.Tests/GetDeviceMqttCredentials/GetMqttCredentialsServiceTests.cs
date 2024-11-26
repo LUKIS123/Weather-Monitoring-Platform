9d0dc5bb43a13ed52b83d87526a1e4f5c5334e92
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2024-11-26 12:27:00 +0100

    unit tests for core microservice done

---
+++ src/WeatherMonitorCore/WeatherMonitorCore.DeviceManagement.Tests/GetDeviceMqttCredentials/GetMqttCredentialsServiceTests.cs
@@ -0,0 +1,152 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using WeatherMonitorCore.Contract.DeviceManagementModule;
+using WeatherMonitorCore.DeviceManagement.Features.GetDeviceMqttCredentials;
+using WeatherMonitorCore.DeviceManagement.Infrastructure;
+using WeatherMonitorCore.DeviceManagement.Infrastructure.Models;
+using WeatherMonitorCore.Interfaces;
+using WeatherMonitorCore.SharedKernel.Exceptions;
+
+namespace WeatherMonitorCore.DeviceManagement.Tests.GetDeviceMqttCredentials;
+
+[TestFixture]
+public class GetMqttCredentialsServiceTests
+{
+    private IDeviceManagementRepository _deviceManagementRepository;
+    private IAesEncryptionHelper _aesEncryptionHelper;
+    private GetMqttCredentialsService _service;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _deviceManagementRepository = Substitute.For<IDeviceManagementRepository>();
+        _aesEncryptionHelper = Substitute.For<IAesEncryptionHelper>();
+        _service = new GetMqttCredentialsService(_deviceManagementRepository, _aesEncryptionHelper);
+    }
+
+    [Test]
+    public async Task Handle_ShouldReturnNotFoundException_WhenDeviceHasMissingFields()
+    {
+        // Arrange
+        var deviceId = 1;
+        var device = new MqttCredentialsDto
+        {
+            Id = deviceId,
+            ClientId = null, // Missing required field
+            Username = "test-user",
+            Password = "encrypted-password",
+            Topic = "test/topic"
+        };
+
+        _deviceManagementRepository.GetDeviceByIdAsync(deviceId).Returns(device);
+
+        // Act
+        var result = await _service.Handle(deviceId);
+
+        // Assert
+        result.Error?.Exception.Should().BeOfType<NotFoundException>();
+        result.Error?.Exception.As<NotFoundException>().Message.Should().Contain($"Device with id:{deviceId} not found");
+    }
+
+    [Test]
+    public async Task Handle_ShouldReturnDeviceMqttCredentialsResponse_WhenDeviceIsValid()
+    {
+        // Arrange
+        var deviceId = 1;
+        var encryptedPassword = "encrypted-password";
+        var decryptedPassword = "plain-text-password";
+        var device = new MqttCredentialsDto
+        {
+            Id = deviceId,
+            ClientId = "client-id",
+            Username = "test-user",
+            Password = encryptedPassword,
+            Topic = "test/topic"
+        };
+
+        _deviceManagementRepository.GetDeviceByIdAsync(deviceId).Returns(device);
+        _aesEncryptionHelper.Decrypt(encryptedPassword).Returns(decryptedPassword);
+
+        // Act
+        var result = (await _service.Handle(deviceId)).Value;
+
+        // Assert
+        result.Should().BeOfType<DeviceMqttCredentialsResponse>();
+        var response = result.As<DeviceMqttCredentialsResponse>();
+        response.Id.Should().Be(deviceId);
+        response.ClientId.Should().Be("client-id");
+        response.Username.Should().Be("test-user");
+        response.Password.Should().Be(decryptedPassword);
+        response.Topic.Should().Be("test/topic");
+    }
+
+    [Test]
+    public async Task Handle_ShouldCallGetDeviceByIdAsync_Once()
+    {
+        // Arrange
+        var deviceId = 1;
+        var device = new MqttCredentialsDto
+        {
+            Id = deviceId,
+            ClientId = "client-id",
+            Username = "test-user",
+            Password = "encrypted-password",
+            Topic = "test/topic"
+        };
+
+        _deviceManagementRepository.GetDeviceByIdAsync(deviceId).Returns(device);
+
+        // Act
+        await _service.Handle(deviceId);
+
+        // Assert
+        await _deviceManagementRepository.Received(1).GetDeviceByIdAsync(deviceId);
+    }
+
+    [Test]
+    public async Task Handle_ShouldCallDecrypt_Once_WhenDevicePasswordIsNotNull()
+    {
+        // Arrange
+        var deviceId = 1;
+        var encryptedPassword = "encrypted-password";
+        var device = new MqttCredentialsDto
+        {
+            Id = deviceId,
+            ClientId = "client-id",
+            Username = "test-user",
+            Password = encryptedPassword,
+            Topic = "test/topic"
+        };
+
+        _deviceManagementRepository.GetDeviceByIdAsync(deviceId).Returns(device);
+
+        // Act
+        await _service.Handle(deviceId);
+
+        // Assert
+        _aesEncryptionHelper.Received(1).Decrypt(encryptedPassword);
+    }
+
+    [Test]
+    public async Task Handle_ShouldNotCallDecrypt_WhenDevicePasswordIsNull()
+    {
+        // Arrange
+        var deviceId = 1;
+        var device = new MqttCredentialsDto
+        {
+            Id = deviceId,
+            ClientId = "client-id",
+            Username = "test-user",
+            Password = null, // Password is null
+            Topic = "test/topic"
+        };
+
+        _deviceManagementRepository.GetDeviceByIdAsync(deviceId).Returns(device);
+
+        // Act
+        var result = await _service.Handle(deviceId);
+
+        // Assert
+        _aesEncryptionHelper.DidNotReceive().Decrypt(Arg.Any<string>());
+    }
+}
---
+++ src/WeatherMonitorCore/WeatherMonitorCore.DeviceManagement.Tests/UpdateDeviceStatus/UpdateDeviceStatusServiceTests.cs
@@ -0,0 +1,135 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using WeatherMonitorCore.Contract.DeviceManagementModule;
+using WeatherMonitorCore.DeviceManagement.Features.UpdateDeviceStatus;
+using WeatherMonitorCore.DeviceManagement.Infrastructure;
+using WeatherMonitorCore.SharedKernel;
+
+namespace WeatherMonitorCore.DeviceManagement.Tests.UpdateDeviceStatus;
+
+[TestFixture]
+public class UpdateDeviceStatusServiceTests
+{
+    private IDeviceManagementRepository _deviceManagementRepository;
+    private UpdateDeviceStatusService _service;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _deviceManagementRepository = Substitute.For<IDeviceManagementRepository>();
+        _service = new UpdateDeviceStatusService(_deviceManagementRepository);
+    }
+
+    [Test]
+    public async Task Handle_ShouldReturnSuccess_WhenNoStationsUpdatesProvided()
+    {
+        // Arrange
+        var request = new UpdateDeviceStatusRequest
+        {
+            StationsUpdates = new List<Contract.DeviceManagementModule.UpdateDeviceStatus>()
+        };
+
+        // Act
+        var result = await _service.Handle(request);
+
+        // Assert
+        result.Should().BeOfType<Result>();
+        result.IsSuccess.Should().BeTrue();
+        await _deviceManagementRepository.DidNotReceiveWithAnyArgs()
+            .BulkUpdateDevicesStatusAsync(default, default);
+    }
+
+    [Test]
+    public async Task Handle_ShouldCallBulkUpdateDevicesStatusAsync_ForEachGroup()
+    {
+        // Arrange
+        var request = new UpdateDeviceStatusRequest
+        {
+            StationsUpdates = new List<Contract.DeviceManagementModule.UpdateDeviceStatus>
+            {
+                new() { DeviceId = 1, SetActive = true },
+                new() { DeviceId = 2, SetActive = false },
+                new() { DeviceId = 3, SetActive = true }
+            }
+        };
+
+        // Act
+        var result = await _service.Handle(request);
+
+        // Assert
+        await _deviceManagementRepository.Received(1)
+            .BulkUpdateDevicesStatusAsync(Arg.Is<int[]>(ids => ids.SequenceEqual(new[] { 1, 3 })), true);
+        await _deviceManagementRepository.Received(1)
+            .BulkUpdateDevicesStatusAsync(Arg.Is<int[]>(ids => ids.SequenceEqual(new[] { 2 })), false);
+        result.Should().BeOfType<Result>();
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [Test]
+    public async Task Handle_ShouldNotCallBulkUpdateDevicesStatusAsync_WhenStationsUpdatesIsEmpty()
+    {
+        // Arrange
+        var request = new UpdateDeviceStatusRequest
+        {
+            StationsUpdates = new List<Contract.DeviceManagementModule.UpdateDeviceStatus>()
+        };
+
+        // Act
+        var result = await _service.Handle(request);
+
+        // Assert
+        await _deviceManagementRepository.DidNotReceiveWithAnyArgs().BulkUpdateDevicesStatusAsync(default, default);
+        result.Should().BeOfType<Result>();
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [Test]
+    public async Task Handle_ShouldHandleMultipleGroupsCorrectly()
+    {
+        // Arrange
+        var request = new UpdateDeviceStatusRequest
+        {
+            StationsUpdates = new List<Contract.DeviceManagementModule.UpdateDeviceStatus>
+            {
+                new() { DeviceId = 1, SetActive = true },
+                new() { DeviceId = 2, SetActive = true },
+                new() { DeviceId = 3, SetActive = false },
+                new() { DeviceId = 4, SetActive = false }
+            }
+        };
+
+        // Act
+        var result = await _service.Handle(request);
+
+        // Assert
+        await _deviceManagementRepository.Received(1)
+            .BulkUpdateDevicesStatusAsync(Arg.Is<int[]>(ids => ids.SequenceEqual(new[] { 1, 2 })), true);
+        await _deviceManagementRepository.Received(1)
+            .BulkUpdateDevicesStatusAsync(Arg.Is<int[]>(ids => ids.SequenceEqual(new[] { 3, 4 })), false);
+        result.Should().BeOfType<Result>();
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [Test]
+    public void Handle_ShouldThrowException_WhenBulkUpdateDevicesStatusAsyncFails()
+    {
+        // Arrange
+        var request = new UpdateDeviceStatusRequest
+        {
+            StationsUpdates = new List<Contract.DeviceManagementModule.UpdateDeviceStatus>
+            {
+                new() { DeviceId = 1, SetActive = true }
+            }
+        };
+
+        _deviceManagementRepository.BulkUpdateDevicesStatusAsync(Arg.Any<int[]>(), Arg.Any<bool>())
+            .Throws(new Exception("Database error"));
+
+        // Act
+        Func<Task> act = async () => await _service.Handle(request);
+
+        // Assert
+        act.Should().ThrowAsync<Exception>().WithMessage("Database error");
+    }
+}
